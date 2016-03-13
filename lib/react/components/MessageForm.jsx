cc1ddacecf2a904f1641dd61bc183325cb7d8b2e
ID25 <xid25x@gmail.com>
2016-03-14 01:36:58 +0300

    Update styles

--- lib/react/components/MessageForm.jsx
+++ lib/react/components/MessageForm.jsx
@@ -14,18 +14,11 @@ export default class MessageForm extends Component {
   render() {
     const { fields: { body, senderClass }, handleSubmit } = this.props;
     return (
-      <div>
+      <div className='denshobato-send-message'>
         <form onSubmit={handleSubmit}>
-          <div className="bottom_wrapper clearfix">
-            <div className="message_input_wrapper">
-              <input className="message_input" placeholder="Type your message here..." {...body}/>
-              </div>
-            <div className="send_message">
-              <div className="icon"></div>
-              <button onclick={handleSubmit} className="text">Send</button>
-            </div>
-          </div>
-      </form>
+          <input className="message_input" placeholder="Type your message here..." {...body}/>
+          <button onclick={handleSubmit} className="text">Send</button>
+        </form>
       </div>
     );
   }
